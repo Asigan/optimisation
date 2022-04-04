8ef117e325e4f22e9ede7c1186e6b741dfb5c888
Asigan <titouanguy@gmail.com>
2022-04-04 12:15:51 +0200

    Ajout classe client

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+#include "Client.hpp"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Client c1 = Client(0, 1, 1, 4);
+    Client c2 = Client(0, 1, 3, 4);
+    std::cout << c1.getDistanceHeuristique(c2) << std::endl;
+    std::cout << c1.getX() << std::endl;
+    std::cout << c1.getY() << std::endl;
+    std::cout << c1.getQuantity() << std::endl;
+
     return 0;
 }
