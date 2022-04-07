554ec5823593f8a5fac295c55e8c323d901ed4b3
Asigan <titouanguy@gmail.com>
2022-04-07 13:07:59 +0200

    Ajout et tests de la classe Tournee

--- Tournee.h
+++ Tournee.h
@@ -4,5 +4,149 @@
 
 #ifndef OPTIMISATION_TOURNEE_H
 #define OPTIMISATION_TOURNEE_H
+using namespace std;
+#include <unordered_map>
+#include <vector>
+#include "Client.hpp"
+
+
+class Tournee{
+public:
+    Tournee(std::vector<Client>& _clients) {
+        predecesseurs = std::unordered_map<int, int>();
+        successeurs = std::unordered_map<int, int>();
+        quantite_restante = 100;
+        distance_heuristique = 0;
+        clients = _clients;
+        predecesseurs.emplace(0, 0);
+        successeurs.emplace(0, 0);
+    }
+
+    void insert(int insertedC, int clientBefore){
+        if(insertedC > clients.size() || quantite_restante < clients[insertedC].getQuantity()){
+            // TODO: ajouter le client dans la liste des clients
+        }
+        else{
+            int clientAfter = successeurs[clientBefore];
+            successeurs[clientBefore] = insertedC;
+            predecesseurs[clientAfter] = insertedC;
+            successeurs.emplace(make_pair(insertedC, clientAfter));
+            predecesseurs.emplace(make_pair(insertedC, clientBefore));
+
+            quantite_restante -= clients[insertedC].getQuantity();
+            distance_heuristique -= getDistance(clientBefore, clientAfter);
+            distance_heuristique += getDistance(clientBefore, insertedC);
+            distance_heuristique += getDistance(clientAfter, insertedC);
+        }
+    }
+
+    void switchClients(int c1, int c2){
+        if(successeurs.count(c1) > 0
+                && successeurs.count(c2) > 0
+                    && c1 != c2
+                        && c1 != 0 && c2 !=0) {
+            int afterC1 = successeurs[c1];
+            int beforeC1 = predecesseurs[c1];
+
+            int afterC2 = successeurs[c2];
+            int beforeC2 = predecesseurs[c2];
+
+            // destruction des liens autour de c1
+            distance_heuristique -= getDistance(c1, beforeC1);
+            distance_heuristique -= getDistance(c1, afterC1);
+            // destruction des liens autour de c2
+            distance_heuristique -= getDistance(c2, beforeC2);
+            distance_heuristique -= getDistance(c2, afterC2);
+
+
+
+            if(afterC1 == c2){
+                afterC1 = c1;
+                beforeC2 = c2;
+            }
+            else if(afterC2 == c1){
+                afterC2 = c2;
+                beforeC1 = c1;
+            }
+
+            successeurs[c1] = afterC2;
+            predecesseurs[c1] = beforeC2;
+
+            successeurs[beforeC2] = c1;
+            predecesseurs[afterC2] = c1;
+
+            successeurs[c2] = afterC1;
+            predecesseurs[c2] = beforeC1;
+
+            successeurs[beforeC1] = c2;
+            predecesseurs[afterC1] = c2;
+
+            // création des liens autour de c1 (à partir des anciennes connexions de c2)
+            distance_heuristique += getDistance(c1, beforeC2);
+            distance_heuristique += getDistance(c1, afterC2);
+            // création des liens autour de c2 (à partir des anciennes connexions de c1)
+            distance_heuristique += getDistance(c2, beforeC1);
+            distance_heuristique += getDistance(c2, afterC1);
+
+
+        }
+    }
+
+    void deleteClient(int deletedC){
+        if(successeurs.count(deletedC) > 0 && predecesseurs.count(deletedC) > 0){
+            int beforeC = predecesseurs[deletedC];
+            int afterC = successeurs[deletedC];
+
+            successeurs[beforeC] = afterC;
+            predecesseurs[afterC] = beforeC;
+            successeurs.erase(deletedC);
+            predecesseurs.erase(deletedC);
+
+            quantite_restante += clients[deletedC].getQuantity();
+            distance_heuristique -= (getDistance(deletedC, beforeC)
+                                     + getDistance(deletedC, afterC));
+            distance_heuristique += getDistance(beforeC, afterC);
+        }
+    }
+
+    int getQuantiteRestante(){
+        return quantite_restante;
+    }
+
+    double getDistanceHeuristique(){
+        return distance_heuristique;
+    }
+
+    std::string toString(){
+        std::string res = "0 -> ";
+        int client = 0;
+        while(successeurs[client] != 0){
+            client = successeurs[client];
+            res += to_string(client) + " -> ";
+        }
+        return res += "0;";
+    }
+
+    vector<int> returnTournee(){
+        vector<int> res = std::vector<int>();
+        int client = 0;
+        while(successeurs[client] != 0){
+            client = successeurs[client];
+            res.push_back(client);
+        }
+        return res;
+    }
+
+private:
+    unordered_map<int, int> predecesseurs;
+    unordered_map<int, int> successeurs;
+    int quantite_restante;
+    double distance_heuristique;
+    std::vector<Client> clients;
+
+    double getDistance(int c1, int c2){
+        return clients[c1].getDistance(clients[c2]);
+    }
+};
 
 #endif //OPTIMISATION_TOURNEE_H
--- main.cpp
+++ main.cpp
@@ -1,12 +1,59 @@
 #include <iostream>
+#include <vector>
 #include "Client.hpp"
+#include "Tournee.h"
+using namespace std;
 int main() {
-    Client c1 = Client(0, 1, 1, 4);
-    Client c2 = Client(0, 1, 3, 4);
-    std::cout << c1.getDistanceHeuristique(c2) << std::endl;
+    Client depot = Client(0, 0, 0, 0);
+    Client c1 = Client(1, 1, 1, 4);
+    Client c2 = Client(2, 1, 3, 4);
+    Client c3 = Client(3, 2, 4, 4);
+
+    /*std::cout << c1.getDistance(c2) << std::endl;
     std::cout << c1.getX() << std::endl;
     std::cout << c1.getY() << std::endl;
-    std::cout << c1.getQuantity() << std::endl;
+    std::cout << c1.getQuantity() << std::endl;*/
+
+    vector<Client> clients =  vector<Client>();
+    clients.push_back(depot);
+    clients.push_back(c1);
+    clients.push_back(c2);
+    clients.push_back(c3);
+
+    Tournee t = Tournee(clients);
+
+    t.insert(1, 0);
+    cout << t.toString() << std::endl;
+    std::cout << t.getQuantiteRestante() << std::endl;
+    std::cout << t.getDistanceHeuristique() << std::endl;
+
+    t.insert(2, 1);
+    cout << t.toString() << std::endl;
+    std::cout << t.getQuantiteRestante() << std::endl;
+    std::cout << t.getDistanceHeuristique() << std::endl;
+
+    t.switchClients(1, 2);
+    cout << t.toString() << std::endl;
+    std::cout << t.getQuantiteRestante() << std::endl;
+    std::cout << t.getDistanceHeuristique() << std::endl;
+
+
+    t.insert(3, 2);
+    cout << t.toString() << std::endl;
+    std::cout << t.getQuantiteRestante() << std::endl;
+    std::cout << t.getDistanceHeuristique() << std::endl;
+
+    t.switchClients(1, 2);
+    cout << t.toString() << std::endl;
+    std::cout << t.getQuantiteRestante() << std::endl;
+    std::cout << t.getDistanceHeuristique() << std::endl;
+
+
+    t.deleteClient(1);
+    cout << t.toString() << std::endl;
+    std::cout << t.getQuantiteRestante() << std::endl;
+    double value = t.getDistanceHeuristique();
+    std::cout << t.getDistanceHeuristique() << std::endl;
 
     return 0;
 }
