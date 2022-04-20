d96f8956d0e84d975ef7af742f090731285a794f
Asigan <titouanguy@gmail.com>
2022-04-20 16:03:09 +0200

    ajout du main pour exemple des dernières modifs

--- main.cpp
+++ main.cpp
@@ -3,57 +3,18 @@
 #include "Client.hpp"
 #include "Tournee.h"
 #include "Reader.h"
+#include "Solution.h"
+
 using namespace std;
 int main() {
-    Client depot = Client(0, 0, 0, 0);
-    Client c1 = Client(1, 1, 1, 4);
-    Client c2 = Client(2, 1, 3, 4);
-    Client c3 = Client(3, 2, 4, 4);
-
-    /*std::cout << c1.getDistance(c2) << std::endl;
-    std::cout << c1.getX() << std::endl;
-    std::cout << c1.getY() << std::endl;
-    std::cout << c1.getQuantity() << std::endl;*/
-
-    vector<Client> clients =  vector<Client>();
-    clients.push_back(depot);
-    clients.push_back(c1);
-    clients.push_back(c2);
-    clients.push_back(c3);
-
-    Tournee t = Tournee(clients);
-
-    t.insert(1, 0);
-    cout << t.toString() << std::endl;
-    std::cout << t.getQuantiteRestante() << std::endl;
-    std::cout << t.getDistanceHeuristique() << std::endl;
-
-    t.insert(2, 1);
-    cout << t.toString() << std::endl;
-    std::cout << t.getQuantiteRestante() << std::endl;
-    std::cout << t.getDistanceHeuristique() << std::endl;
-
-    t.switchClients(1, 2);
-    cout << t.toString() << std::endl;
-    std::cout << t.getQuantiteRestante() << std::endl;
-    std::cout << t.getDistanceHeuristique() << std::endl;
-
-
-    t.insert(3, 2);
-    cout << t.toString() << std::endl;
-    std::cout << t.getQuantiteRestante() << std::endl;
-    std::cout << t.getDistanceHeuristique() << std::endl;
-
-    t.switchClients(1, 2);
-    cout << t.toString() << std::endl;
-    std::cout << t.getQuantiteRestante() << std::endl;
-    std::cout << t.getDistanceHeuristique() << std::endl;
-
-
-    t.deleteClient(1);
-    cout << t.toString() << std::endl;
-    std::cout << t.getQuantiteRestante() << std::endl;
-    std::cout << t.getDistanceHeuristique() << std::endl;
+    Reader r = Reader();
+    std::vector<Client> clients = r.lectureFichier("..\\Tests\\A3205.txt");
+    Solution s = Solution(clients);
+    std::cout << s.toString() << endl;
+    auto missing_clients = s.checkAllClientsAreInSolution(clients);
+    for(auto mc = missing_clients.begin(); mc != missing_clients.end(); ++mc){
+        cout << to_string(*mc) << " - ";
+    }
 
     return 0;
 }
